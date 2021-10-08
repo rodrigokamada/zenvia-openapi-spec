385f6dbd6a3443685ff65e19d9ed92655ca61659
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-10-07 21:57:40 -0300

    Changing the API version.

--- spec/info/index.ts
+++ spec/info/index.ts
@@ -4,7 +4,7 @@ import { rawLoad } from '../../utils/raw-load';
 const description = rawLoad(__dirname, './description.md');
 
 const info: InfoObject = {
-  version: '1.0.0',
+  version: '1.0.1',
   title: 'ZenAPI',
   description,
   termsOfService: 'https://zenvia.com/',
